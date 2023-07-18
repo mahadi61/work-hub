cb7d531dffedb3bb68a5fb87d49c9fd83e73b902
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-07-18 12:35:49 +0600

    conflict resoleve in page.js

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,17 @@
 import Banner from "@/blocks/Banner/Banner";
-import ClientSays from "@/blocks/ClientSays/ClientSays";
-import { Navbar } from "@/components/Navbar/Navbar";
-import TrustedBy from "@/blocks/TrustedBy/TrustedBy";
 import Blogs from "@/blocks/Blogs/Blogs";
+import ClientSays from "@/blocks/ClientSays/ClientSays";
+import TrustedBy from "@/blocks/TrustedBy/TrustedBy";
+import { Navbar } from "@/components/Navbar/Navbar";
 
 const Home = () => {
   return (
     <main>
+      <Navbar />
       <Banner />
       <FeaturedJobs />
+      <TrustedBy />
+      <Blogs />
       <ClientSays></ClientSays>
     </main>
   );
