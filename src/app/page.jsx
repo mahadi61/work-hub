d926bc48c9cb91fba4074a0d965a57d3625ae9ad
Mahadi Hasan <mahadihasan8461@gmail.com>
2023-07-18 12:32:26 +0600

    footer added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,15 @@
-import FeaturedJobs from "@/blocks/FeaturedJobs/FeaturedJobs";
 import Banner from "@/blocks/Banner/Banner";
 import ClientSays from "@/blocks/ClientSays/ClientSays";
+import FeaturedJobs from "@/blocks/FeaturedJobs/FeaturedJobs";
+import Footer from "@/blocks/Footer/Footer";
 
 const Home = () => {
   return (
     <main>
-      <Banner/>
-      <FeaturedJobs/>
-      <ClientSays></ClientSays>
+      <Banner />
+      <FeaturedJobs />
+      <ClientSays />
+      <Footer />
     </main>
   );
 };
